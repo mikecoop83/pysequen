ef2c9f4a400bbf9c4c82eb72c2c6dada28ac7442
Michael Coopersmith <mikecoop83@gmail.com>
2016-07-29 14:45:32 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pysequen',
+      version='0.1',
+      description='Python library for concurrent processing of tasks with dependencies requiring sequencing',
+      url='https://github.com/mikecoop83/pysequen/',
+      author='Michael Coopersmith',
+      author_email='mikecoop83@gmail.com',
+      license='MIT',
+      packages=['pysequen'],
+      install_requires=[
+          'gevent',
+          'python-slimta'
+      ],
+      zip_safe=False)
